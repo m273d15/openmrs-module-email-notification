6de64d37309f2099b77046380eca7edbf4eb6250
Hamza <hamza@Hamzas-MacBook-Pro.local>
2020-09-15 17:18:31 +0530

    [Hamza] [#56] Added Apache Commons Configuration to load environment variables through props

--- api/src/main/java/org/openmrs/module/email/notification/service/EmailNotificationService.java
+++ api/src/main/java/org/openmrs/module/email/notification/service/EmailNotificationService.java
@@ -1,7 +1,8 @@
 package org.openmrs.module.email.notification.service;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.mail.EmailException;
 
 public interface EmailNotificationService {
-    void sendEmail(String recipientAddress, String subject, String body) throws EmailException;
+    void sendEmail(String recipientAddress, String subject, String body) throws EmailException, ConfigurationException;
 }
--- api/src/main/java/org/openmrs/module/email/notification/service/impl/EmailNotificationServiceImpl.java
+++ api/src/main/java/org/openmrs/module/email/notification/service/impl/EmailNotificationServiceImpl.java
@@ -1,15 +1,13 @@
 package org.openmrs.module.email.notification.service.impl;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
 import org.openmrs.module.email.notification.service.EmailNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @Service("emailNotificationService")
 public class EmailNotificationServiceImpl implements EmailNotificationService {
@@ -20,38 +18,21 @@ public class EmailNotificationServiceImpl implements EmailNotificationService {
         this.htmlEmail = htmlEmail;
     }
 
-    private Properties loadConfig() {
-        String propFileName = "config.properties";
-        Properties emailConfig = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-        try {
-            emailConfig.load(inputStream);
-            inputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return emailConfig;
-    }
+    public void sendEmail(String recipientAddress, String subject, String body) throws EmailException, ConfigurationException {
+        PropertiesConfiguration emailConfig = new PropertiesConfiguration("config.properties");
 
-    public void sendEmail(String recipientAddress, String subject, String body) throws EmailException {
-        Properties emailConfig = loadConfig();
-
-        try {
-            htmlEmail.setHostName(emailConfig.getProperty("host"));
-            htmlEmail.setAuthentication(
-                    System.getenv("AWS_SMTP_USERNAME"),
-                    System.getenv("AWS_SMTP_PASSWORD")
-            );
-            htmlEmail.setSmtpPort(Integer.parseInt(emailConfig.getProperty("port")));
-            htmlEmail.setSSLOnConnect(true);
-            htmlEmail.addTo(recipientAddress);
-            htmlEmail.setFrom(emailConfig.getProperty("from"), emailConfig.getProperty("fromName"));
-            htmlEmail.setSubject(subject);
-            htmlEmail.setHtmlMsg(body);
-            htmlEmail.send();
-        } catch (EmailException ex) {
-            System.out.println(ex);
-        }
+        htmlEmail.setHostName(emailConfig.getString("HOST"));
+        htmlEmail.setAuthentication(
+                emailConfig.getString("SMTP_USERNAME"),
+                emailConfig.getString("SMTP_PASSWORD")
+        );
+        htmlEmail.setSmtpPort(emailConfig.getInt("PORT"));
+        htmlEmail.setSSLOnConnect(true);
+        htmlEmail.addTo(recipientAddress);
+        htmlEmail.setFrom(emailConfig.getString("FROM"), emailConfig.getString("FROM_NAME"));
+        htmlEmail.setSubject(subject);
+        htmlEmail.setHtmlMsg(body);
+        htmlEmail.send();
     }
 
     @Autowired
