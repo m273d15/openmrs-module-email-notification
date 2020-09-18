e5343fcd03f14d0a18376051ad4d54a8505c0c97
Hamza <hamza@Hamzas-MacBook-Pro.local>
2020-09-18 11:08:15 +0530

    [Hamza][#56] Removed unused imports

--- api/src/main/java/org/openmrs/module/email/notification/service/EmailNotificationService.java
+++ api/src/main/java/org/openmrs/module/email/notification/service/EmailNotificationService.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.email.notification.service;
 
-import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.mail.EmailException;
 
 public interface EmailNotificationService {
